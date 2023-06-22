0780e1481d12037b749d8e1f8d66f7c32521816b
coderbuggy <coderbuggy@gmailcom>
2023-06-22 22:41:58 +0300

    global context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,44 @@ import bg from '../src/img/bg.png'
 import {MainLayout} from './styles/Layout'
 import Orb from './Components/Orb/Orb'
 import Navigation from './Components/Navigation/Navigation'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Income from './Components/Incomes/Incomes'
+import Expenses from './Components/Expenses/Expenses'
+import { useGlobaContext } from './context/globalContext'
 
 function App() {
   const [active, setActive] = useState(1)
 
+  const global = useGlobaContext()
+  console.log(global);
+
   const orbMemo = useMemo(() => {
     return <Orb/>
   }, [])
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
+  }
 
   return (
     <AppStyled bg={bg} className='App'>
      {orbMemo}
        <MainLayout>
         <Navigation active={active} setActive={setActive}/>
+        <main>
+          {displayData()}
+        </main>
        </MainLayout>
     </AppStyled>
   )
@@ -26,6 +51,18 @@ function App() {
 const AppStyled = styled.div`
 height: 100vh;
 background-image: url(${props => props.bg});
+main{
+  flex: 1;
+  background: rgba(252,246,249,0.78);
+  border: 3px solid #FFFFFF;
+  backdrop-filter: blur(4.5px);
+  border-radius: 32px;
+  overflow: auto;
+  overflow-x: hidden;
+  &&::-webkit-scrollbar{
+    width: 0
+  }
+}
 `;
 
 
---
+++ frontend/src/Components/Expenses/Expenses.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { InnerLayout } from '../../styles/Layout'
+
+function Expenses() {
+  return (
+    <ExpensesStyled>
+        <InnerLayout>
+
+        </InnerLayout>
+    </ExpensesStyled>
+  )
+}
+
+
+const ExpensesStyled = styled.div`
+
+
+
+`
+
+export default Expenses
